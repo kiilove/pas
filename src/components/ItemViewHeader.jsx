dddfe0e4dd98b5c3f9bfe840ecaec2c2cbf724c7
kiilove <jbkim@jncore.com>
2023-11-27 16:41:27 +0900

    commit on "main"

--- src/components/ItemViewHeader.jsx
+++ src/components/ItemViewHeader.jsx
@@ -7,11 +7,22 @@ const ItemViewHeader = ({ scrollPosition }) => {
   return (
     <div className="flex w-full h-10 justify-start items-center bg-white">
       <div className="flex justify-start items-center w-full h-full">
-        <FaArrowLeftLong
-          onClick={() => {
-            window.history.back();
-          }}
-        />
+        <div className="flex w-1/3">
+          <FaArrowLeftLong
+            onClick={() => {
+              window.history.back();
+            }}
+          />
+        </div>
+        <div className="flex w-1/3 justify-center items-center">
+          <span
+            className="text-gray-900 font-semibold"
+            style={{ fontFamily: "Noto Sans KR", fontSize: "16px" }}
+          >
+            상품보기
+          </span>
+        </div>
+        <div className="flex w-1/3"></div>
       </div>
     </div>
   );
